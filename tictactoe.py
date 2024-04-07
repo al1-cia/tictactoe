b7b2acf608fedb882fc4054b3d31e49c6a5998b8
Caron Sally Pereira <92998380+norac1243@users.noreply.github.com>
2024-04-07 23:45:49 +0530

    Made 5 changes to the original code  (#1)
    * #CHANGE 1: chnaged the way dictt was initialized. Removed the zeros in each list since it aint required, cause we just append the numbers to the list and having position 0 in it is a problem since it assumes positon 0 is occupied by either X or O
    
    * #CHANGE 2: Added an inner if statement that checks if the position of the mark (X or O) is already in the dictt. if it is not already there, then the position is appended to the respective lists in the dictt
    
    * #CHANGE 2: Added an inChanged the elif to if (in the block of code where we are appending the lists in dictt with positions of the respective marks). Did not understand why exactly we use elif in this situation.
    
    * CHANGE4 -- changed the code segment under if(np.abs(maxx)==1 or np.abs(maxx)==0): to facilitate random first move of the player O
    
    * CHANGE5 -- added a break to avoid placing O in two places on the grid (note: not sure if the problem of double moves is because of it cause the problem still persists)

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,6 @@
 import numpy as np
+import random 
+
 #computer's turn
 def disp():
     for i in range(0,9):
@@ -11,9 +13,14 @@ def comp():
     #store taken positions
     for block in range(0,9):
         if (arr[block]=='X'):
-            dictt['X'].append(block)
-        elif (arr[block]=='O'):
-            dictt['O'].append(block)
+             #CHANGE2 -- added inner if statement
+            if block not in dictt['X']:
+                dictt['X'].append(block)
+        #CHANGE3 -- changed elif to if 
+        if (arr[block]=='O'):
+             #CHANGE2 -- added inner if statement
+            if block not in dictt['O']:
+                dictt['O'].append(block)
 
     for gstate, wt in goal.items():
         counter=0
@@ -32,18 +39,34 @@ def comp():
             maxx=wt
    
     #who's about to win?
-    if(np.abs(maxx)==1 or np.abs(maxx)==0): #try to randomize
+    """
+    f(np.abs(maxx)==1 or np.abs(maxx)==0): #this is in case of first move,try to randomize
         for pos in range(0,9):
             if(arr[pos]==0):
                 arr[pos]='O'
                 break
-
+    """
+    #CHANGE4 -- changed the entire bit above in comments to the one below
+    #was done so that the first move of O does not get fixed to just position 0 of the grid.#
+    #better to randomise the first move of the comp to any position 
+    #also added import random statement above
+    checker=0
+    if(np.abs(maxx)==1 or np.abs(maxx)==0): #this is in case of first move,try to randomize
+        while checker==0:  
+            pos = random.randint(0,8)    
+            if(arr[pos]==0): 
+                arr[pos]='O'
+                checker=1
+            
     elif(maxx==2):
         for gstate,wt in goal.items():
             if(wt==2):
                 for elem in gstate:
                     if(elem not in dictt['O']):
                         arr[elem]='O'
+                        #CHANGE5 -- added a break to avoid placing O in two places
+                        #not sure if the problem of double moves is because of it cause the problem still persists
+                        break
 
     elif(maxx==-2):
         for gstate,wt in goal.items():
@@ -51,6 +74,9 @@ def comp():
                 for elem in gstate:
                     if(elem not in dictt['X']):
                         arr[elem]='O'
+                        #CHANGE5 -- added a break to avoid placing O in two places
+                        #not sure if the problem of double moves is because of it cause the problem still persists
+                        break
 
     if(np.abs(maxx)==3):
         if(maxx==3):print("GAME OVER")
@@ -81,7 +107,9 @@ def usinp():
 #initialize
 arr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 goal={(0,4,8):0,(1,4,7):0,(0,1,2):0,(2,4,6):0,(0,3,6):0,(3,4,5):0,(2,5,8):0,(6,7,8):0}
-dictt = {'X': [0,0,0,0,0,0,0,0,0], 'O': [0,0,0,0,0,0,0,0,0]}  #taken positions
+#~ dictt = {'X': [0,0,0,0,0,0,0,0,0], 'O': [0,0,0,0,0,0,0,0,0]}  #taken positions
+#CHANGE 1, changed the above to the below since it was unnecessary to have the 0s present
+dictt = {'X': [], 'O': []}
 counter=0
 a=0
 disp()
