64d08c1be7720587d25a46256f71738b7b4f10d5
norac1243 <phicsp1234@gmail.com>
2024-05-08 06:37:14 +0530

    buttons dont work that well

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -5,24 +5,27 @@ import random
 pygame.init()
 
 # Set up the screen
-WIDTH, HEIGHT = 1000, 770
+WIDTH, HEIGHT = 900, 700
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic-Tac-Toe")
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 DARK_YELLOW = (204, 204, 0)
 NEON_ORANGE = (255, 165, 0)
+PURPLE = (128, 0, 128)
+GREEN = (0, 128, 0)
+DARK_BLUE = (0, 0, 128)
 
-# Centering the grid 
-GRID_WIDTH = 300  # Width of the grid
-GRID_HEIGHT = 300  # Height of the grid
+# Centering the grid
+GRID_WIDTH = 300
+GRID_HEIGHT = 300
 GRID_X = (WIDTH - GRID_WIDTH) // 2
-GRID_Y = (HEIGHT - GRID_HEIGHT) // 2 - 50  # Shifting the entire layout 50 pixels above
+GRID_Y = (HEIGHT - GRID_HEIGHT) // 2 - 50
 
 # Fonts
 message_font = pygame.font.SysFont("Times New Roman", 40)
@@ -33,28 +36,37 @@ arr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 goal = {(0, 4, 8): 0, (1, 4, 7): 0, (0, 1, 2): 0, (2, 4, 6): 0, (0, 3, 6): 0, (3, 4, 5): 0, (2, 5, 8): 0, (6, 7, 8): 0}
 dictt = {'X': [], 'O': []}
 a = 0  # Game over flag
+player_score = 0
+computer_score = 0
 
-# Functions
+# Define button dimensions and spacing
+BUTTON_WIDTH = 180
+BUTTON_HEIGHT = 80
+BUTTON_SPACE_TO_EDGE = 50
+BUTTON_SPACING = 26
+
 def disp():
+    global score_button, play_again_button, restart_game_button, quit_button
+
     SCREEN.fill(WHITE)
 
     # Draw Tic Tac Toe heading
     heading_font = pygame.font.SysFont("Times New Roman", 100)
-    heading_text = heading_font.render("Tic Tac Toe", True, NEON_ORANGE)  # Neon Orange color
-    heading_rect = heading_text.get_rect(center=(WIDTH // 2, GRID_Y - 100))  # Positioned above the grid
+    heading_text = heading_font.render("Tic Tac Toe", True, NEON_ORANGE)
+    heading_rect = heading_text.get_rect(center=(WIDTH // 2, GRID_Y - 100))
     SCREEN.blit(heading_text, heading_rect)
 
     for i in range(0, 9):
-        cell_size = GRID_WIDTH // 3  # Calculate the size of each cell based on the grid dimensions
-        row = i // 3  # Calculate the row index of the cell
-        col = i % 3  # Calculate the column index of the cell
-        x = GRID_X + col * cell_size  # Calculate the x-coordinate of the top-left corner of the cell
-        y = GRID_Y + row * cell_size  # Calculate the y-coordinate of the top-left corner of the cell
-        pygame.draw.rect(SCREEN, YELLOW, pygame.Rect(x, y, 100, 100))  # Draw grid background
+        cell_size = GRID_WIDTH // 3
+        row = i // 3
+        col = i % 3
+        x = GRID_X + col * cell_size
+        y = GRID_Y + row * cell_size
+        pygame.draw.rect(SCREEN, YELLOW, pygame.Rect(x, y, 100, 100))
 
-        # Draw thicker grid lines
+        # Draw grid lines
         line_width = 3
-        pygame.draw.rect(SCREEN, DARK_YELLOW, pygame.Rect(x, y, 100, 100), line_width)  # Draw grid lines
+        pygame.draw.rect(SCREEN, DARK_YELLOW, pygame.Rect(x, y, 100, 100), line_width)
 
         if arr[i] == 'X':
             text = symbol_font.render('X', True, RED)
@@ -65,10 +77,11 @@ def disp():
             text_rect = text.get_rect(center=(x + cell_size // 2, y + cell_size // 2))
             SCREEN.blit(text, text_rect)
 
-    # Draw thicker outer border of the grid
+    # Draw  outer border of the grid
     border_rect = pygame.Rect(GRID_X - 2, GRID_Y - 2, GRID_WIDTH + 4, GRID_HEIGHT + 4)
-    pygame.draw.rect(SCREEN, DARK_YELLOW, border_rect, line_width) 
+    pygame.draw.rect(SCREEN, DARK_YELLOW, border_rect, line_width)
 
+    # Draw message
     if a == 1:
         message = message_font.render("Game Over", True, BLACK)
         SCREEN.blit(message, (WIDTH // 2 - 90, GRID_Y + GRID_HEIGHT + 10))
@@ -78,7 +91,52 @@ def disp():
     elif a == 3:
         message = message_font.render("Draw", True, BLACK)
         SCREEN.blit(message, (WIDTH // 2 - 40, GRID_Y + GRID_HEIGHT + 10))
+
+    # Draw score button
+    score_button = pygame.Rect(BUTTON_SPACE_TO_EDGE, HEIGHT - BUTTON_HEIGHT - BUTTON_SPACE_TO_EDGE, BUTTON_WIDTH, BUTTON_HEIGHT)
+    pygame.draw.rect(SCREEN, GREEN, score_button, border_radius=10)
+    score_text_line1 = message_font.render("X: " + str(player_score), True, WHITE)
+    score_text_line2 = message_font.render("O: " + str(computer_score), True, WHITE)
+    score_text_rect_line1 = score_text_line1.get_rect(center=(score_button.center[0], score_button.center[1] - 20))
+    score_text_rect_line2 = score_text_line2.get_rect(center=(score_button.center[0], score_button.center[1] + 20))
+    SCREEN.blit(score_text_line1, score_text_rect_line1)
+    SCREEN.blit(score_text_line2, score_text_rect_line2)
+
+    # Draw play again button
+    play_again_button = pygame.Rect(BUTTON_SPACE_TO_EDGE + BUTTON_WIDTH + BUTTON_SPACING, HEIGHT - BUTTON_HEIGHT - BUTTON_SPACE_TO_EDGE, BUTTON_WIDTH, BUTTON_HEIGHT)
+    pygame.draw.rect(SCREEN, RED, play_again_button, border_radius=10)
+    play_again_text_line1 = message_font.render("Play", True, WHITE)
+    play_again_text_line2 = message_font.render("Again", True, WHITE)
+    play_again_text_rect1 = play_again_text_line1.get_rect(center=(play_again_button.center[0], play_again_button.center[1] - 20))
+    play_again_text_rect2 = play_again_text_line2.get_rect(center=(play_again_button.center[0], play_again_button.center[1] + 20))
+    SCREEN.blit(play_again_text_line1, play_again_text_rect1)
+    SCREEN.blit(play_again_text_line2, play_again_text_rect2)
+
+    # Draw restart game button
+    restart_game_button = pygame.Rect(BUTTON_SPACE_TO_EDGE + 2 * (BUTTON_WIDTH + BUTTON_SPACING), HEIGHT - BUTTON_HEIGHT - BUTTON_SPACE_TO_EDGE, BUTTON_WIDTH, BUTTON_HEIGHT)
+    pygame.draw.rect(SCREEN, DARK_BLUE, restart_game_button, border_radius=10)
+    restart_game_text_line1 = message_font.render("Restart", True, WHITE)
+    restart_game_text_line2 = message_font.render("Game", True, WHITE)
+    restart_game_text_rect1 = restart_game_text_line1.get_rect(center=(restart_game_button.center[0], restart_game_button.center[1] - 20))
+    restart_game_text_rect2 = restart_game_text_line2.get_rect(center=(restart_game_button.center[0], restart_game_button.center[1] + 20))
+    SCREEN.blit(restart_game_text_line1, restart_game_text_rect1)
+    SCREEN.blit(restart_game_text_line2, restart_game_text_rect2)
+
+    # Draw quit button
+    quit_button = pygame.Rect(WIDTH - BUTTON_SPACE_TO_EDGE - BUTTON_WIDTH, HEIGHT - BUTTON_HEIGHT - BUTTON_SPACE_TO_EDGE, BUTTON_WIDTH, BUTTON_HEIGHT)
+    pygame.draw.rect(SCREEN, PURPLE, quit_button, border_radius=10)
+    quit_text = message_font.render("Quit", True, WHITE)
+    quit_text_rect = quit_text.get_rect(center=quit_button.center)
+    SCREEN.blit(quit_text, quit_text_rect)
+
     pygame.display.flip()
+
+
+def reset_game():
+    global arr, a, dictt
+    arr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    dictt = {'X': [], 'O': []}
+    a = 0
 
 def maxfind():
     global arr
@@ -107,14 +165,16 @@ def maxfind():
     return maxx
 
 def end():
-    global a
+    global a, player_score, computer_score
     maxx = maxfind()
     if abs(maxx) == 3:
         if maxx == 3:
             a = 1
+            computer_score += 1
         else:
             a = 2
-        print("Game state updated to:", a)  # Debug statement
+            player_score += 1
+        print("Game state updated to:", a)
         return
 
     counterr = 0
@@ -123,7 +183,7 @@ def end():
             counterr += 1
     if counterr == 9:
         a = 3
-        print("Game state updated to:", a)  # Debug statement
+        print("Game state updated to:", a)
         return
 
 def comp():
@@ -153,14 +213,13 @@ def comp():
                         return
 
 def usinp(pos):
-    row = pos // 3  # Calculate the row index of the cell
-    col = pos % 3   # Calculate the column index of the cell
-    if arr[pos] == 0:
-        arr[pos] = 'X'
-        end()
-        return
-    else:
-        print("Place is taken.\n")
+    if 0 <= pos <= 8:
+        row = pos // 3
+        col = pos % 3
+        if arr[pos] == 0:
+            arr[pos] = 'X'
+            end()
+            return
 
 # Game Loop
 running = True
@@ -171,7 +230,6 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN and a == 0:
             pos = pygame.mouse.get_pos()
-            # Coordinates of the cell that the user has clicked on
             x, y = pos[0] - GRID_X, pos[1] - GRID_Y
             cell_size = GRID_WIDTH // 3
             cell_x = x // cell_size
@@ -182,6 +240,16 @@ while running:
             if a != 0:
                 continue
             comp()
-            disp()
-
+            disp()# Inside the game loop
+        elif event.type == pygame.MOUSEBUTTONDOWN and a != 0:
+            if quit_button.collidepoint(event.pos):
+                running = False  # Set running to False to exit the game loop
+            elif play_again_button.collidepoint(event.pos):
+                reset_game()
+                disp()
+            elif restart_game_button.collidepoint(event.pos):
+                reset_game()
+                player_score = 0
+                computer_score = 0
+                disp()
 pygame.quit()
--- working tictactoe.py
+++ working tictactoe.py
@@ -12,11 +12,21 @@ pygame.display.set_caption("Tic-Tac-Toe")
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-BLUE=(0,0,255)
-RED=(255,0,0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+DARK_YELLOW = (204, 204, 0)
+NEON_ORANGE = (255, 165, 0)
+
+# Centering the grid 
+GRID_WIDTH = 300  # Width of the grid
+GRID_HEIGHT = 300  # Height of the grid
+GRID_X = (WIDTH - GRID_WIDTH) // 2
+GRID_Y = (HEIGHT - GRID_HEIGHT) // 2 - 50  # Shifting the entire layout 50 pixels above
 
 # Fonts
-FONT = pygame.font.SysFont(None, 40)
+message_font = pygame.font.SysFont("Times New Roman", 40)
+symbol_font = pygame.font.SysFont("Times New Roman", 40, bold=True)
 
 # Game variables
 arr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -28,12 +38,11 @@ a = 0  # Game over flag
 def disp():
     SCREEN.fill(WHITE)
 
-    #centering the grid 
-    GRID_WIDTH = 300  # Width of the grid
-    GRID_HEIGHT = 300  # Height of the grid
-    GRID_X = (WIDTH - GRID_WIDTH) // 2
-    GRID_Y = (HEIGHT - GRID_HEIGHT) // 2
-
+    # Draw Tic Tac Toe heading
+    heading_font = pygame.font.SysFont("Times New Roman", 100)
+    heading_text = heading_font.render("Tic Tac Toe", True, NEON_ORANGE)  # Neon Orange color
+    heading_rect = heading_text.get_rect(center=(WIDTH // 2, GRID_Y - 100))  # Positioned above the grid
+    SCREEN.blit(heading_text, heading_rect)
 
     for i in range(0, 9):
         cell_size = GRID_WIDTH // 3  # Calculate the size of each cell based on the grid dimensions
@@ -41,23 +50,34 @@ def disp():
         col = i % 3  # Calculate the column index of the cell
         x = GRID_X + col * cell_size  # Calculate the x-coordinate of the top-left corner of the cell
         y = GRID_Y + row * cell_size  # Calculate the y-coordinate of the top-left corner of the cell
+        pygame.draw.rect(SCREEN, YELLOW, pygame.Rect(x, y, 100, 100))  # Draw grid background
+
+        # Draw thicker grid lines
+        line_width = 3
+        pygame.draw.rect(SCREEN, DARK_YELLOW, pygame.Rect(x, y, 100, 100), line_width)  # Draw grid lines
+
         if arr[i] == 'X':
-            text = FONT.render('X', True, RED)
+            text = symbol_font.render('X', True, RED)
             text_rect = text.get_rect(center=(x + cell_size // 2, y + cell_size // 2))
             SCREEN.blit(text, text_rect)
         elif arr[i] == 'O':
-            text = FONT.render('O', True, BLUE)
-            SCREEN.blit(text, (x + cell_size // 2 - 20, y + cell_size // 2 - 20))  # Draw the 'O' text at the center of the cell
-        pygame.draw.rect(SCREEN, BLACK, pygame.Rect(x, y, 100, 100), 2)
+            text = symbol_font.render('O', True, BLUE)
+            text_rect = text.get_rect(center=(x + cell_size // 2, y + cell_size // 2))
+            SCREEN.blit(text, text_rect)
+
+    # Draw thicker outer border of the grid
+    border_rect = pygame.Rect(GRID_X - 2, GRID_Y - 2, GRID_WIDTH + 4, GRID_HEIGHT + 4)
+    pygame.draw.rect(SCREEN, DARK_YELLOW, border_rect, line_width) 
+
     if a == 1:
-        message = FONT.render("Game Over", True, BLACK)
-        SCREEN.blit(message, (WIDTH // 2 - 60, GRID_Y + GRID_HEIGHT + 10))
+        message = message_font.render("Game Over", True, BLACK)
+        SCREEN.blit(message, (WIDTH // 2 - 90, GRID_Y + GRID_HEIGHT + 10))
     elif a == 2:
-        message = FONT.render("You Win", True, BLACK)
-        SCREEN.blit(message, (WIDTH // 2 - 50, GRID_Y + GRID_HEIGHT + 10))
+        message = message_font.render("You Win", True, BLACK)
+        SCREEN.blit(message, (WIDTH // 2 - 70, GRID_Y + GRID_HEIGHT + 10))
     elif a == 3:
-        message = FONT.render("Draw", True, BLACK)
-        SCREEN.blit(message, (WIDTH // 2 - 30, GRID_Y + GRID_HEIGHT + 10))
+        message = message_font.render("Draw", True, BLACK)
+        SCREEN.blit(message, (WIDTH // 2 - 40, GRID_Y + GRID_HEIGHT + 10))
     pygame.display.flip()
 
 def maxfind():
@@ -131,6 +151,8 @@ def comp():
                         return
 
 def usinp(pos):
+    row = pos // 3  # Calculate the row index of the cell
+    col = pos % 3   # Calculate the column index of the cell
     if arr[pos] == 0:
         arr[pos] = 'X'
         end()
@@ -147,10 +169,13 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN and a == 0:
             pos = pygame.mouse.get_pos()
-            #coordinates of the cell that the user has clicked on
-            x, y = pos[0] // 100, pos[1] // 100
-            #passing index of the cell that the user has clicked on 
-            usinp(y * 3 + x)
+            # Coordinates of the cell that the user has clicked on
+            x, y = pos[0] - GRID_X, pos[1] - GRID_Y
+            cell_size = GRID_WIDTH // 3
+            cell_x = x // cell_size
+            cell_y = y // cell_size
+            cell_index = cell_y * 3 + cell_x
+            usinp(cell_index)
             disp()
             if a != 0:
                 continue
